589a01e9122e4f8f68386b19ccf75d9203605015
Michał Bartczak <bartczak1234@gmail.com>
2023-09-03 13:40:48 +0200

    deploy of application on the aws cloud

--- src/main/java/pl/coderslab/magazyn/MagazynApplication.java
+++ src/main/java/pl/coderslab/magazyn/MagazynApplication.java
@@ -1,20 +1,21 @@
 package pl.coderslab.magazyn;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import pl.coderslab.magazyn.entity.Employee;
-import pl.coderslab.magazyn.repository.EmployeeRepository;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MagazynApplication {
+public class MagazynApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MagazynApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MagazynApplication.class, args);
+    }
 
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MagazynApplication.class);
+    }
 }
 
